299056323aac3335819fddd9b6e7ee4343f83869
Marta <43724620+gulidamarta@users.noreply.github.com>
2020-09-19 20:27:38 +0300

    update comment from rus to eng language

--- lab1/V7_2.sql
+++ lab1/V7_2.sql
@@ -2,22 +2,22 @@ USE AdventureWorks2012;
 GO
 
 /*
-¬ывести на экран количество отделов, которые вход€т в группу 'Executive General and Administration'
+Display on the screen amount of departments, which belongs to the group 'Executive General and Administration'
 */
 
 SELECT COUNT([DepartmentID]) as DepartmentCount FROM HumanResources.Department WHERE [GroupName] = 'Executive General and Administration';
 GO
 
 /*
-¬ывести на экран 5(п€ть) самых молодых сотрудников.
+Display on the screen 5 (five) the youngest employees.
 */
 SELECT TOP 5 [BusinessEntityID], [JobTitle], [Gender], [BirthDate] FROM HumanResources.Employee 
 ORDER BY [BirthDate] DESC;
 GO
 
 /*
-¬ывести на экран список сотрудников женского пола, прин€тых на работу во вторник (Tuesday). 
-¬ поле [LoginID] заменить домен Сadventure-worksТ на Сadventure-works2012Т
+Display a list of female employees hired on Tuesday.
+In the [LoginID] field, replace the Сadventure-worksТ domain with Сadventure-works2012Т
 */ 
 SELECT [BusinessEntityID], [JobTitle], [Gender], [HireDate], 
 REPLACE([LoginID], 'adventure-works', 'adventure-works2012') 
